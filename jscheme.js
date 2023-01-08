e17fa31d30022192d6dd10c27a258175c7523777
labstraction <labstraction@protonmail.com>
2023-01-08 01:36:01 +0100

    array fns

--- jscheme.js
+++ jscheme.js
@@ -1,51 +1,53 @@
 function first(array) {
-    if (isArray(array) && !isEmpty(array)) {
-        return array[0]
-    } else {
-        return null;
-    }
+  return isArray(array) && !isEmpty(array) ? array[0] : null;
 }
 
 function isArray(array) {
-    return Array.isArray(array);
+  return Array.isArray(array);
 }
 
 function isEmpty(array) {
-    return array ? array.length === 0 : true;
+  return isArray(array) ? array.length === 0 : true;
 }
 
 function rest(array) {
-    if (isArray(array)) {
-        return array.slice(1);
-    } else {
-        return null;
-    }
+  return isArray(array) ? array.slice(1) : null;
 }
 
-function cons(element, array) {
-    if (isArray(array)) {
-        return [element, ...array];
-    } else {
-        return null;
-    }
+function unshift(element, array) {
+  return isArray(array) ? [element, ...array] : null;
 }
 
-function isObject(object) {
-    return typeof object === 'object';
-}
 
 function eq(a, b) {
-   return Object.is(a, b);
+  return Object.is(a, b);
 }
 
 function member(element, array) {
-    if (isArray(array)) {
-        return array.includes(element);
-    } else {
-        return false;
-    }
+  return isArray(array) ? array.includes(element) : null;
+}
+
+function map(fn, array) {
+  return isArray(array) ? array.map(fn) : null;
+}
+
+function filter(fn, array) {
+  return isArray(array) ? array.filter(fn) : null;
+}
+
+function reduce(fn, array, initial) {
+  return isArray(array) ? array.reduce(fn, initial) : null;
+}
+
+function reduceR(fn, array, initial) {
+  return isArray(array) ? array.reduceRight(fn, initial) : null;
+}
+
+function pipe(value, ...fns) {
+  return fns.reduce((acc, fn) => fn(acc), value);
 }
 
 
 
-export {first, rest, isArray, isEmpty, cons, isObject, eq, member};
+
+export { first, rest, isArray, isEmpty, unshift, eq, member, map, filter, reduce, reduceR, pipe };
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { isArray, isEmpty, first, rest, cons } from './jscheme'
+import { isArray, isEmpty, first, rest, unshift, pipe } from './jscheme'
 
 console.log(isArray([1, 2, 3]))
 console.log(isEmpty([1, 2, 3]))
@@ -7,7 +7,13 @@ console.log(first([1, 2, 3]))
 console.log(rest([1, 2, 3]))
 console.log(rest([1]))
 console.log(rest([]))
-console.log(cons(1, [2, 3]))
+console.log(unshift(1, [2, 3]))
+
+console.log(pipe([1, 2, 3],
+                 first,
+                 x => x + 1, 
+                 x => x * 2, 
+                 x => x - 1))
 
 
 
