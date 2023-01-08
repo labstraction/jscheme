94ce69125cd1da5bf2c80aaec8e217b509d7ccbe
labstraction <labstraction@protonmail.com>
2023-01-08 02:21:00 +0100

    array fns 2

--- jscheme.js
+++ jscheme.js
@@ -18,12 +18,11 @@ function unshift(element, array) {
   return isArray(array) ? [element, ...array] : null;
 }
 
-
 function eq(a, b) {
   return Object.is(a, b);
 }
 
-function member(element, array) {
+function includes(element, array) {
   return isArray(array) ? array.includes(element) : null;
 }
 
@@ -47,7 +46,88 @@ function pipe(value, ...fns) {
   return fns.reduce((acc, fn) => fn(acc), value);
 }
 
+function some(fn, array) {
+  return isArray(array) ? array.some(fn) : null;
+}
+
+function every(fn, array) {
+  return isArray(array) ? array.every(fn) : null;
+}
+
+function concat(array1, array2) {
+  return isArray(array1) && isArray(array2) ? array1.concat(array2) : null;
+}
+
+function join(separator, array) {
+  return isArray(array) ? array.join(separator) : null;
+}
+
+function mapValues(fn, obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[key] = fn(obj[key]);
+    return acc;
+  }, {});
+}
+
+function mapKeys(fn, obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[fn(key)] = obj[key];
+    return acc;
+  }, {});
+}
+
+function filterValues(fn, obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    if (fn(obj[key])) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {});
+}
+
+function filterKeys(fn, obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    if (fn(key)) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {});
+}
+
+function reduceValues(fn, obj, initial) {
+  return Object.keys(obj).reduce((acc, key) => {
+    return fn(acc, obj[key]);
+  }, initial);
+}
+
+function reduceKeys(fn, obj, initial) {
+  return Object.keys(obj).reduce((acc, key) => {
+    return fn(acc, key);
+  }, initial);
+}
 
 
-
-export { first, rest, isArray, isEmpty, unshift, eq, member, map, filter, reduce, reduceR, pipe };
+export {
+  first,
+  rest,
+  isArray,
+  isEmpty, 
+  unshift, 
+  eq, 
+  includes, 
+  map, 
+  filter, 
+  reduce, 
+  reduceR, 
+  pipe, 
+  some, 
+  every, 
+  concat, 
+  join, 
+  mapValues, 
+  mapKeys, 
+  filterValues, 
+  filterKeys, 
+  reduceValues, 
+  reduceKeys
+};
